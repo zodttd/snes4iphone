6e33fd800c00fe2acb7e9427d803259ece1e3b75
zodttd <zodttd@zodttd.com>
2009-08-30 16:28:02 -0400

    v5.0.0

--- Classes/TabBar.h
+++
@@ -1,7 +0,0 @@
-@interface TabBar : UITabBar {}
--(void)resetButton;
--(void)didMoveToWindow;
-//-(void)didMoveToWindowNowPlaying;
-//-(void)didMoveToWindowBookmarks;
--(void)didMoveToWindowSaveStates;
-@end
